398d0e7c56c9b956aa714b7ddb6578c58f0c05c3
Mikhail_Miroliubov <Mikhail_Miroliubov@epam.com>
2018-03-07 18:57:22 +0300

    Testing in progress

---
+++ test/services/accounting/DefaultAccountServiceSpec.scala
@@ -0,0 +1,32 @@
+package services.accounting
+
+import java.util.Date
+
+import entities.Account
+import org.junit.runner.RunWith
+import org.specs2.mock.Mockito
+import org.specs2.mutable._
+import org.specs2.runner.JUnitRunner
+import repositories.AccountingRepository
+
+import scala.concurrent.duration.Duration
+import scala.concurrent.{Await, Future}
+
+@RunWith(classOf[JUnitRunner])
+class DefaultAccountServiceSpec extends Specification with Mockito {
+  val testAccount1 = Account(Some(1), "testOwner", 100.0, Some(new Date()))
+  val testAccount2 = Account(Some(2), "testOwner", 100.0, Some(new Date()))
+
+  val repositoryMock = mock[AccountingRepository]
+  repositoryMock.getAccountById(testAccount1.id.get) returns Future.successful(Some(testAccount1))
+  repositoryMock.getAccountById(testAccount2.id.get) returns Future.successful(Some(testAccount2))
+
+  val accountingService = new DefaultAccountService(repositoryMock)
+
+  "DefaultAccountService" should {
+    "create transaction" in {
+      val transaction = Await.result(accountingService.createTransaction(testAccount1.id, testAccount2.id, 50.0), Duration.Inf)
+      transaction.id.isDefined must beTrue
+    }
+  }
+}
